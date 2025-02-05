6a005b348d9c3476e109b6b220ff852b080620a5
carince <99774021+carince@users.noreply.github.com>
2025-02-05 19:00:02 +0800

    remove unnecessary motion stuff

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { Item } from '@/types';
 import { useIsTouchDevice } from '@/utils/touch';
-import { motion, useMotionValue, useMotionValueEvent, useVelocity } from 'motion/react';
+import { motion } from 'motion/react';
 
 function Draggable({ item, onDragStart, onDragStop }: React.PropsWithoutRef<{
     item: Item,
@@ -38,18 +38,11 @@ function Draggable({ item, onDragStart, onDragStop }: React.PropsWithoutRef<{
                 : ""
         : "";
 
-    const x = useMotionValue(0)
-    const xVelocity = useVelocity(x)
-
-    useMotionValueEvent(xVelocity, "change", latest => {
-        console.log("Velocity", latest)
-    })
-
     return (
         <motion.div
             ref={item.style ? ref : null}
             className={`flex flex-row items-stretch text-xs md:text-[16px] leading-5 border-2 border-neutral-200 rounded-xl backdrop-blur-sm select-none w-36 md:w-48 transition-shadow ${hover}`}
-            style={{ ...pos(), x }}
+            style={{ ...pos() }}
             {...item.style ? { ...onProps } : {}}
             initial={{ opacity: 0, scale: 1, }}
             animate={{ opacity: 1, scale: 1 }}
