7847e2dbdd9bd5c47b98fd6e8d426268d84bce3f
carince <99774021+carince@users.noreply.github.com>
2025-02-12 00:09:25 +0800

    remove zod

--- src/schemas/auth.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const authSchema = z.object({
-    email: z.string().email().refine((email) => /^[a-zA-Z0-9._%+-]+@ija\.edu\.ph$/.test(email), {
-        message: 'Email must be a valid ija.edu.ph email address',
-    }),
-    password: z.string().min(6, 'Password must be at least 6 characters long'),
-});
