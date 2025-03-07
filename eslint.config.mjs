5be3a76af6aa58cda83672b149a18bbd79432ec8
carince <99774021+carince@users.noreply.github.com>
2025-03-07 09:02:41 +0800

    fix: disable react/no-unescaped-entities rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ const eslintConfig = [
       "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
       "import/order": [
         "error",
         {
